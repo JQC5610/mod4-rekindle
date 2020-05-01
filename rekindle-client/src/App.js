1c1158495f3605b22c88215121943405709b8c8c
zachbrown12 <zachmb22@gmail.com>
2020-05-01 13:12:07 -0400

    Fetch requests working

--- rekindle-client/src/App.js
+++ rekindle-client/src/App.js
@@ -1,69 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import * as SpotifyWebApi from 'spotify-web-api-js';
 
-import SpotifyWebApi from 'spotify-web-api-js';
-const spotifyApi = new SpotifyWebApi();
+let spotify = new SpotifyWebApi();
 
-
-class App extends Component {
+class App extends React.Component {
   constructor(){
     super();
     const params = this.getHashParams();
-    const token = params.access_token;
-    if (token) {
-      spotifyApi.setAccessToken(token);
-    }
+
     this.state = {
-      loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
+      loggedIn: params.access_token ? true : false,
+      nowPlaying: {
+        name: "Not Checked",
+        image: ''
+      }
+    }
+    if (params.access_token) {
+      spotify.setAccessToken(params.access_token)
     }
   }
 
-  getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
-    }
-    return hashParams;
+
+
+
+getHashParams = () => {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+  while ( e = r.exec(q)) {
+     hashParams[e[1]] = decodeURIComponent(e[2]);
   }
+  return hashParams;
+}
 
-  getNowPlaying(){
-    fetch('https://api.spotify.com/v1/me/player/currently-playing')
-      .then((response) => {
-        this.setState({
-          nowPlaying: { 
-              name: response.item.name, 
-              albumArt: response.item.album.images[0].url
-            }
-        });
-      })
-  }
-
-
-
-  render() {
-    return (
-      <div className="App">
-        <a href='http://localhost:8888' > Login to Spotify </a>
-        <div>
-          Now Playing: { this.state.nowPlaying.name }
-        </div>
-        <div>
-          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
-        </div>
-        { this.state.loggedIn &&
-          <button onClick={() => this.getNowPlaying()}>
-            Check Now Playing
-          </button>
+getNowPlaying = () => {
+  spotify.getMyCurrentPlaybackState()
+    .then((response) => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          image: response.item.album.images[0].url
         }
+      })
+    })
+}
+
+getRecentlyPlayed = () => {
+  spotify.getMyRecentlyPlayedTracks()
+    .then((response) => {
+      console.log(response)
+    })
+};
+
+getMyTopTracks = () => {
+  spotify.getMyTopTracks()
+    .then((response) => {
+      console.log(response)
+    })
+};
+
+render() {
+  return (
+    <div className="App">
+      <a href='http://localhost:8888'>
+        <button>Login with Spotify</button>
+      </a>
+      <div>
+        Now Playing: { this.state.nowPlaying.name } 
       </div>
-    );
-  }
+      <div>
+        <img src = {this.state.nowPlaying.image} style={{ width: 100}}/>
+      </div>
+      <button onClick={() => this.getNowPlaying()}>
+        Now Playing
+      </button>
+      <button onClick={() => this.getRecentlyPlayed()}>
+        Recent Songs
+      </button>
+      <button onClick={() => this.getMyTopTracks()}>
+        Top Tracks
+      </button>
+
+
+    </div> 
+  );
+}
+
 }
 
 export default App;
